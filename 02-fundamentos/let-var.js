5f06dcb7e95b9d056639b54d2860ef06a9e8bcc2
cjgmj <cjgmj@hotmail.com>
2019-12-27 13:01:45 +0100

    Let vs Var

---
+++ 02-fundamentos/let-var.js
@@ -0,0 +1,27 @@
+var exVar = 'Wolverine';
+
+if (true) {
+    var exVar = 'Magneto';
+}
+
+console.log(exVar);
+
+let exLet = 'Wolverine';
+
+// Crea la variable en un contexto distinto
+if (true) {
+    let exLet = 'Magneto';
+}
+
+console.log(exLet);
+
+for (var i = 0; i <= 5; i++) {
+    console.log(`i: ${i}`);
+}
+
+console.log(i);
+
+// j no se puede usar fuera del for
+for (let j = 0; j <= 5; j++) {
+    console.log(`j: ${j}`);
+}
