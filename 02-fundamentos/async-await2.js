ea158610fe01060397f0321ef195f82f514ce87c
cjgmj <cjgmj@hotmail.com>
2019-12-29 13:58:51 +0100

    Resolver problema empleados/salarios con Async-Await

---
+++ 02-fundamentos/async-await2.js
@@ -0,0 +1,49 @@
+/*jshint esversion: 8 */
+
+let empleados = [{
+    id: 1,
+    nombre: 'John'
+}, {
+    id: 2,
+    nombre: 'Joe'
+}, {
+    id: 3,
+    nombre: 'Jane'
+}];
+
+let salarios = [{
+    id: 1,
+    salario: 1000
+}, {
+    id: 2,
+    salario: 2000
+}];
+
+let getEmpleado = async(id) => {
+    let empleadoDB = empleados.find(empleado => empleado.id === id);
+
+    if (!empleadoDB) {
+        throw new Error(`No existe un empleado con el ID ${id}`);
+    } else {
+        return empleadoDB;
+    }
+};
+
+let getSalario = async(empleado) => {
+    let salarioDB = salarios.find(salario => salario.id === empleado.id);
+
+    if (!salarioDB) {
+        throw new Error(`No se encontró un salario para el empleado ${empleado.nombre}`);
+    } else {
+        return { nombre: empleado.nombre, salario: salarioDB.salario, id: empleado.id };
+    }
+};
+
+let getInformacion = async(id) => {
+    let empleado = await getEmpleado(id);
+    let resp = await getSalario(empleado);
+
+    return `El salario de ${resp.nombre} es de ${resp.salario}€`;
+};
+
+getInformacion(2).then(mensaje => console.log(mensaje)).catch(err => console.log(err.message));
