324d509b0b99c14ef70deb20343a7e51fe6b5418
cjgmj <cjgmj@hotmail.com>
2019-12-31 14:28:34 +0100

    Crear archivo con resultado

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,7 +1,20 @@
 /*jshint esversion: 6 */
 
+// requireds
+const fs = require('fs');
+
 let base = 2;
+let data = '';
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`${base} * ${i} = ${base * i}`);
+    data += `${base} * ${i} = ${base * i}`;
+
+    if (i != 10) {
+        data += '\n';
+    }
 }
+
+fs.writeFile(`tabla-${base}.txt`, data, (err) => {
+    if (err) throw err;
+    console.log(`El archivo tabla-${base}.txt ha sido creado`);
+});
