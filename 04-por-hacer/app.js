dc7a47d2f257471607ecc5e07e05de710ae5f59d
cjgmj <cjgmj@hotmail.com>
2020-01-02 17:31:32 +0100

    Base aplicación por hacer

---
+++ 04-por-hacer/app.js
@@ -0,0 +1,22 @@
+/*jshint esversion: 6 */
+
+const argv = require('yargs').argv;
+
+console.log(argv);
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'crear':
+        console.log('Crear tarea');
+        break;
+    case 'listar':
+        console.log('Listar tareas');
+        break;
+    case 'actualizar':
+        console.log('Actualizar tarea');
+        break;
+    default:
+        console.log('Comando no reconocido');
+        break;
+}
