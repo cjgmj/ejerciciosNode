b6fcfb6910e8e7fd013d8e842e6a93b282431157
cjgmj <cjgmj@hotmail.com>
2020-01-04 16:27:20 +0100

    Despliegue en Heroku

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -6,6 +6,8 @@ const app = express();
 const hbs = require('hbs');
 require('./hbs/helpers');
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
 
 // Express HBS engine
@@ -22,4 +24,4 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.listen(3000, () => console.log('Escuchando el puerto 3000'));
+app.listen(port, () => console.log(`Escuchando el puerto ${port}`));
