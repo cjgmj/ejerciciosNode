e30b3852c4d6a1e507e46c60613814630dadc2a1
cjgmj <cjgmj@hotmail.com>
2020-01-07 12:16:49 +0100

    Ordenar las rutas de Usuario

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,4 +1,5 @@
 /*jshint esversion: 8 */
+
 require('./config/config');
 
 const express = require('express');
@@ -14,35 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
-
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es obligatorio'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
+app.use(require('./routes/usuario'));
 
 mongoose.connect('mongodb://localhost:27017/cafe', {
     useNewUrlParser: true,
