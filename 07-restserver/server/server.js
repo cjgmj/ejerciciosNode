0c59b9150c7e8642dc24f382b06913536c0dcd87
cjgmj <cjgmj@hotmail.com>
2020-01-07 10:46:49 +0100

    Base aplicación RESTServer

---
+++ 07-restserver/server/server.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 8 */
+
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.json('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Escuchando puerto:', 3000);
+});
