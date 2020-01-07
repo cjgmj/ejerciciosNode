cb99463eff80dd8653bc5c8068a6679f00b24d06
cjgmj <cjgmj@hotmail.com>
2020-01-07 12:37:02 +0100

    Crear usuario

--- 07-restserver/server/routes/usuario.js
+++ 07-restserver/server/routes/usuario.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 8 */
 
 const express = require('express');
+const Usuario = require('../models/usuario');
 
 const app = express();
 
@@ -11,16 +12,26 @@ app.get('/usuario', function(req, res) {
 app.post('/usuario', function(req, res) {
     let body = req.body;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es obligatorio'
-        });
-    } else {
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password,
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
         res.json({
-            persona: body
+            ok: true,
+            usuario: usuarioDB
         });
-    }
+    });
 });
 
 app.put('/usuario/:id', function(req, res) {
