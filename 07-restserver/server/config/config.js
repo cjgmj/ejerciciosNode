177bdf6d76dbe0d38091f32332ca06249098f07c
cjgmj <cjgmj@hotmail.com>
2020-01-08 12:39:39 +0100

    Conexión con Mongo Atlas desde producción

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -2,3 +2,17 @@
 
 // Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Conexión base de datos
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://<user>:<password>@cluster0-a2uzr.mongodb.net/test';
+}
+
+process.env.URLDB = urlDB;
