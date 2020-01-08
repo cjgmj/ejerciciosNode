ec58fbb440270fc89c2a50139c08256250842120
cjgmj <cjgmj@hotmail.com>
2020-01-08 12:56:49 +0100

    Variables de entorno personalizadas Heroku

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -12,7 +12,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://<user>:<password>@cluster0-a2uzr.mongodb.net/test';
+    // urlDB = 'mongodb+srv://<user>:<password>@cluster0-a2uzr.mongodb.net/test';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
