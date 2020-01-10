55e83270aa595d92e507eb1c6e3f4aa14e37bf4b
cjgmj <cjgmj@hotmail.com>
2020-01-10 11:47:17 +0100

    Crear usuario basado en Google

--- 07-restserver/server/routes/login.js
+++ 07-restserver/server/routes/login.js
@@ -65,16 +65,80 @@ async function verify(token) {
     });
     const payload = ticket.getPayload();
 
-    console.log(payload);
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    };
 }
 
-app.post('/google', (req, res) => {
+app.post('/google', async(req, res) => {
     let token = req.body.idtoken;
 
-    verify(token);
+    let googleUser = await verify(token).catch(err => {
+        return res.status(403).json({
+            ok: false,
+            err
+        });
+    });
 
-    res.json({
-        token
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (usuarioDB) {
+            if (!usuarioDB.google) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Debe de usar la autenticación normal'
+                    }
+                });
+            } else {
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+            }
+        } else {
+            // El usuario no existen en base de datos
+            let usuario = new Usuario();
+
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = 'googleLogin';
+
+            usuario.save((err, usuarioDB) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    });
+                }
+
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+            });
+        }
     });
 });
 
