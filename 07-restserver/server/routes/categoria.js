8d0f136b17fb1b2a3662b3cb20a58540fa5c5c99
cjgmj <cjgmj@hotmail.com>
2020-01-10 13:24:50 +0100

    CRUD Categorías

---
+++ 07-restserver/server/routes/categoria.js
@@ -0,0 +1,138 @@
+/*jshint esversion: 8 */
+
+const express = require('express');
+
+let { verificaToken, verificaAdminRole } = require('../middlewares/autenticacion');
+
+let app = express();
+
+let Categoria = require('../models/categoria');
+
+// Mostrar todas las categorías
+app.get('/categoria', verificaToken, (req, res) => {
+    Categoria.find({}, (err, categorias) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categorias
+        });
+    });
+});
+
+// Mostrar categoría por id
+app.get('/categoria/:id', verificaToken, (req, res) => {
+    let id = req.params.id;
+
+    Categoria.findById(id, (err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Categoría no encontrada'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+});
+
+// Crear nueva categoría
+app.post('/categoria', verificaToken, (req, res) => {
+    let body = req.body;
+
+    let categoria = new Categoria({
+        descripcion: body.descripcion,
+        usuario: req.usuario._id
+    });
+
+    categoria.save((err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+});
+
+// Actualizar una categoría
+app.put('/categoria/:id', verificaToken, (req, res) => {
+    let id = req.params.id;
+    let body = req.body;
+
+    Categoria.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Categoría no encontrada'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+});
+
+// Eliminar una categoría
+app.delete('/categoria/:id', [verificaToken, verificaAdminRole], (req, res) => {
+    let id = req.params.id;
+
+    Categoria.findByIdAndRemove(id, (err, categoriaBorrada) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaBorrada) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Categoría no encontrada'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaBorrada,
+            message: 'Categoría eliminada con éxito'
+        });
+    });
+});
+
+module.exports = app;
