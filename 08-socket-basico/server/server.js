76f4929b195a69694dc4ffd2d32dd32db940f91c
cjgmj <cjgmj@hotmail.com>
2020-01-11 16:36:44 +0100

    Instalación socket.io

--- 08-socket-basico/server/server.js
+++ 08-socket-basico/server/server.js
@@ -2,18 +2,25 @@
 
 const express = require('express');
 
+const socketIO = require('socket.io');
+
+const http = require('http');
+
 const path = require('path');
 
 const app = express();
+
+let server = http.createServer(app);
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+// IO mantendrá una comunicación directa con el backend
+let io = socketIO(server);
 
-
-app.listen(port, (err) => {
+server.listen(port, (err) => {
 
     if (err) throw new Error(err);
 
