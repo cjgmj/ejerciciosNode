d606fba7e7f22817875622ae648072a2f3f65bc9
cjgmj <cjgmj@hotmail.com>
2020-01-11 12:19:04 +0100

    Ubicar y renombrar archivos

--- 07-restserver/server/routes/upload.js
+++ 07-restserver/server/routes/upload.js
@@ -4,14 +4,31 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
 
+const Usuario = require('../models/usuario');
+
 app.use(fileUpload({ useTempFiles: true }));
 
-app.put('/upload', function(req, res) {
+app.put('/upload/:tipo/:id', function(req, res) {
+    let tipo = req.params.tipo;
+    let id = req.params.id;
+
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).json({
             ok: false,
             err: {
                 message: 'No se ha seleccionado ningún archivo'
+            }
+        });
+    }
+
+    // Validar tipo
+    let tiposValidos = ['productos', 'usuarios'];
+
+    if (tiposValidos.indexOf(tipo) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Los tipos permitidos son ' + tiposValidos.join(', ')
             }
         });
     }
@@ -33,7 +50,10 @@ app.put('/upload', function(req, res) {
         });
     }
 
-    archivo.mv(`uploads/${archivo.name}`, (err) => {
+    // Cambiar nombre al archivo
+    let nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extension}`;
+
+    archivo.mv(`uploads/${tipo}/${nombreArchivo}`, (err) => {
         if (err)
             return res.status(500).json({
                 ok: false,
