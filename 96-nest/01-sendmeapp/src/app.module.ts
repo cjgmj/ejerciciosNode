07f6b5789b48a144bdf6f66026d335db328374b6
cjgmj <cjgmj@hotmail.com>
2020-12-01 16:16:54 +0100

    Conexión base de datos

--- 96-nest/01-sendmeapp/src/app.module.ts
+++ 96-nest/01-sendmeapp/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MensajesController } from './mensajes/mensajes.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'sendmeapp_db',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, MensajesController],
   providers: [AppService],
 })
