f1346afbeb6849f263d68d314f2d36deba526430
cjgmj <cjgmj@hotmail.com>
2020-12-06 12:49:03 +0100

    Arrays

---
+++ 97-curso-max/02-javascript/arrays.js
@@ -0,0 +1,8 @@
+const hobbies = ["Sports", "Cooking"];
+
+// for (const hobby of hobbies) {
+//   console.log(hobby);
+// }
+
+console.log(hobbies.map((hobby) => "Hobby: " + hobby)); // Map no modifica el array original
+console.log(hobbies);
