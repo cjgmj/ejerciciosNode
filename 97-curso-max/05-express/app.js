6aa69852a5726fce7f296dec2d13c00ba7263327
cjgmj <cjgmj@hotmail.com>
2020-12-17 16:06:21 +0100

    Middleware

--- 97-curso-max/05-express/app.js
+++ 97-curso-max/05-express/app.js
@@ -1,8 +1,22 @@
 const http = require('http');
 
 const express = require('express');
+const { reset } = require('nodemon');
 
 const app = express();
+
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  next(); // Permite a la petición a continuar al siguiente middleware
+  // Si no se llama a la función la petición muere sin enviar respuesta
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+
+  // Si no se llama a la función debemos devolver una respuesta.
+  res.send('<h1>Hello from Express!</h1>');
+});
 
 const server = http.createServer(app);
 
