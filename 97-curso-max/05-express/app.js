19896c1a4d7081063357b10b42f74ade3e8d5d85
cjgmj <cjgmj@hotmail.com>
2020-12-20 11:08:05 +0100

    Servir archivos estáticos

--- 97-curso-max/05-express/app.js
+++ 97-curso-max/05-express/app.js
@@ -15,6 +15,9 @@ app.use(
   })
 );
 
+// Cada vez que se busque un archivo css, js o imágenes se sitúa en la carpeta public
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
