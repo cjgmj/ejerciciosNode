3ba8fa2d483114026d5f9ee43e7de48edfe71fd0
cjgmj <cjgmj@hotmail.com>
2020-12-22 15:56:59 +0100

    Base aplicación contenido dinámico

---
+++ 97-curso-max/06-contenido-dinamico/app.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// Middleware para parsear el cuerpo de las peticiones
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+// Cada vez que se busque un archivo css, js o imágenes se sitúa en la carpeta public
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+// El path no es el absoluto, se indica que debe comenzar por el especificado
+// Es importante el orden de los middleware porque se ejecutan de arriba hacia abajo
+app.use('/', (req, res, next) => {
+  console.log('This always runs!');
+  next();
+});
+
+app.listen(3000);
