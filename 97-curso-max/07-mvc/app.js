dc69ac8a988d26fce5fde270be0f5c54efe888d1
cjgmj <cjgmj@hotmail.com>
2020-12-28 16:14:03 +0100

    Añadiendo controladores

--- 97-curso-max/07-mvc/app.js
+++ 97-curso-max/07-mvc/app.js
@@ -3,12 +3,14 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 // Middleware para parsear el cuerpo de las peticiones
@@ -21,12 +23,10 @@ app.use(
 // Cada vez que se busque un archivo css, js o imágenes se sitúa en la carpeta public
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 // El path no es el absoluto, se indica que debe comenzar por el especificado
 // Es importante el orden de los middleware porque se ejecutan de arriba hacia abajo
--- 97-curso-max/07-mvc/routes/admin.js
+++ 97-curso-max/07-mvc/routes/admin.js
@@ -1,24 +1,13 @@
 const express = require('express');
 
+const productsController = require('../controllers/products');
+
 const router = express.Router();
 
-const products = [];
+// /admin/add-product
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-  });
-});
+router.post('/add-product', productsController.postAddProduct);
 
-// /admin/add-product
-router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  products.push({ title: req.body.title });
-
-  res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
