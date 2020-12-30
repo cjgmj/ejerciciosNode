a93ddc82269b4382c27760f5b0f35015a15b03bc
cjgmj <cjgmj@hotmail.com>
2020-12-30 16:50:53 +0100

    Base aplicación tienda

---
+++ 97-curso-max/08-tienda/models/product.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
+const file = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(file, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+
+      fs.writeFile(file, JSON.stringify(products), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
