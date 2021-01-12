c77ce2694e7007fb957583bb9efe6939e937120b
cjgmj <cjgmj@hotmail.com>
2021-01-12 16:29:27 +0100

    Mostrar datos de un producto específico

--- 97-curso-max/08-tienda/controllers/shop.js
+++ 97-curso-max/08-tienda/controllers/shop.js
@@ -13,9 +13,13 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
 
-  console.log(prodId);
-
-  res.redirect('/');
+  Product.findById(prodId, (product) => {
+    res.render('shop/product-detail', {
+      product,
+      pageTitle: product.title,
+      path: '/products',
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
