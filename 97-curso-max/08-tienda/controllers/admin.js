53ae1cf0f6557a6b4cce41de0723bcd97013840b
cjgmj <cjgmj@hotmail.com>
2021-01-14 15:56:40 +0100

    Cargando datos en la pantalla de edición

--- 97-curso-max/08-tienda/controllers/admin.js
+++ 97-curso-max/08-tienda/controllers/admin.js
@@ -25,10 +25,18 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
 
-  res.render('admin/edit-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/edit-product',
-    editing: editMode,
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
+
+    res.render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product,
+    });
   });
 };
 
