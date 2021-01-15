a1c7f9714dbdcc6c54c8d251aed91d8a8cf34330
cjgmj <cjgmj@hotmail.com>
2021-01-15 14:44:34 +0100

    Eliminar producto

--- 97-curso-max/08-tienda/models/product.js
+++ 97-curso-max/08-tienda/models/product.js
@@ -58,6 +58,17 @@ module.exports = class Product {
     });
   }
 
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const updatedProducts = products.filter((p) => p.id !== id);
+
+      fs.writeFile(file, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+        }
+      });
+    });
+  }
+
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
