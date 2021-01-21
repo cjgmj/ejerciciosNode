fd3df965a06afc80094f69fd8419dce87e6c809f
cjgmj <cjgmj@hotmail.com>
2021-01-21 15:36:05 +0100

    Sincronizar modelo con la base de datos

--- 97-curso-max/09-tienda-sql/app.js
+++ 97-curso-max/09-tienda-sql/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -35,4 +36,10 @@ app.use('/', (req, res, next) => {
   next();
 });
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
