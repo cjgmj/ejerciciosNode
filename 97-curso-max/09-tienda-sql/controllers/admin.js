bfac1d86f520a4fbda6957b13169c35ec17cf610
cjgmj <cjgmj@hotmail.com>
2021-01-22 14:48:01 +0100

    Obteniendo productos desde la pantalla del administrador

--- 97-curso-max/09-tienda-sql/controllers/admin.js
+++ 97-curso-max/09-tienda-sql/controllers/admin.js
@@ -67,13 +67,15 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render('admin/product-list', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products',
-    });
-  });
+  Product.findAll()
+    .then((products) => {
+      res.render('admin/product-list', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products',
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
