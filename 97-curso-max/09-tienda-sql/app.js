d2dd177e12168ddc6447f55b47fe55a6220aee95
cjgmj <cjgmj@hotmail.com>
2021-02-05 15:39:33 +0100

    Modelo de pedido

--- 97-curso-max/09-tienda-sql/app.js
+++ 97-curso-max/09-tienda-sql/app.js
@@ -9,6 +9,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -61,6 +63,13 @@ Cart.belongsTo(User);
 // Relación del carrito con productos
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+
+// Relación de los pedidos con usuario
+Order.belongsTo(User);
+User.hasMany(Order);
+
+// Relación de los productos con pedidos
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
   // .sync(
