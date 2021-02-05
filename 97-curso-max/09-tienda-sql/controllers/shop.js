f3f2ab9ad10e00de3927f7ae723ccf714c21a685
cjgmj <cjgmj@hotmail.com>
2021-02-05 16:21:06 +0100

    Vaciar carrito y mostrar pedidos

--- 97-curso-max/09-tienda-sql/controllers/shop.js
+++ 97-curso-max/09-tienda-sql/controllers/shop.js
@@ -121,22 +121,29 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  let prods;
+
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
-      return req.user.createOrder().then((order) => {
-        return order
-          .addProducts(
-            products.map((product) => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          )
-          .catch((err) => console.log(err));
-      });
+      prods = products;
+      return req.user.createOrder();
+    })
+    .then((order) => {
+      return order.addProducts(
+        prods.map((product) => {
+          product.orderItem = { quantity: product.cartItem.quantity };
+          return product;
+        })
+      );
+    })
+    .then(() => {
+      return fetchedCart.setProducts(null);
     })
     .then(() => {
       res.redirect('/orders');
@@ -145,10 +152,16 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    pageTitle: 'Your Orders',
-    path: '/orders',
-  });
+  req.user
+    .getOrders({ include: ['products'] })
+    .then((orders) => {
+      res.render('shop/orders', {
+        pageTitle: 'Your Orders',
+        path: '/orders',
+        orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
