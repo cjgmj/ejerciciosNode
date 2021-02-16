9dfb934acbce8060de53a4a4dfe20eeb77ba7d9c
cjgmj <cjgmj@hotmail.com>
2021-02-16 14:46:32 +0100

    Crear producto

--- 97-curso-max/10-tienda-nosql/controllers/admin.js
+++ 97-curso-max/10-tienda-nosql/controllers/admin.js
@@ -1,3 +1,5 @@
+const Product = require('../models/product');
+
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
@@ -10,6 +12,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+
+  const product = new Product(title, price, description, imageUrl);
+
+  product
+    .save()
+    .then((result) => {
+      // console.log(result);
+      console.log('Created Product');
+      res.redirect('/admin/products');
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {};
