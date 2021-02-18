426f2525ae85ca50d9c5f3f97d68fd1fe11813b0
cjgmj <cjgmj@hotmail.com>
2021-02-18 15:01:47 +0100

    Eliminar producto

--- 97-curso-max/10-tienda-nosql/controllers/admin.js
+++ 97-curso-max/10-tienda-nosql/controllers/admin.js
@@ -85,4 +85,13 @@ exports.getProducts = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-exports.postDeleteProduct = (req, res, next) => {};
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  Product.deleteById(prodId)
+    .then(() => {
+      console.log('Destroyed product');
+      res.redirect('/admin/products');
+    })
+    .catch((err) => console.log(err));
+};
--- 97-curso-max/10-tienda-nosql/models/product.js
+++ 97-curso-max/10-tienda-nosql/models/product.js
@@ -57,6 +57,18 @@ class Product {
       })
       .catch((err) => console.log(err));
   }
+
+  static deleteById(prodId) {
+    const db = getDb();
+
+    return db
+      .collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => console.log(err));
+  }
 }
 
 module.exports = Product;
