e7af59e1e8e58e1f7a0f2f1fe7a6a626b63d85e8
cjgmj <cjgmj@hotmail.com>
2021-02-18 14:56:01 +0100

    Editar productos

--- 97-curso-max/10-tienda-nosql/controllers/admin.js
+++ 97-curso-max/10-tienda-nosql/controllers/admin.js
@@ -1,4 +1,8 @@
+const mongodb = require('mongodb');
+
 const Product = require('../models/product');
+
+const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -25,9 +29,53 @@ exports.postAddProduct = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-exports.getEditProduct = (req, res, next) => {};
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
 
-exports.postEditProduct = (req, res, next) => {};
+  if (!editMode) {
+    return res.redirect('/');
+  }
+
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return res.redirect('/');
+      }
+
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    new ObjectId(prodId)
+  );
+
+  product
+    .save()
+    .then((result) => {
+      console.log('Updated Product');
+      res.redirect('/admin/products');
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
--- 97-curso-max/10-tienda-nosql/models/product.js
+++ 97-curso-max/10-tienda-nosql/models/product.js
@@ -1,20 +1,29 @@
-const mongobd = require('mongodb');
+const mongodb = require('mongodb');
 
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
+    let dbOp;
+
+    if (this._id) {
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+
+    return dbOp
       .then((result) => console.log(result))
       .catch((err) => console.log(err));
   }
@@ -40,7 +49,7 @@ class Product {
     const db = getDb();
     return db
       .collection('products')
-      .find({ _id: new mongobd.ObjectId(prodId) })
+      .find({ _id: new mongodb.ObjectId(prodId) })
       .next()
       .then((result) => {
         console.log(result);
