33c049672407c3ed2234c46df22d2da554930da4
cjgmj <cjgmj@hotmail.com>
2021-03-10 15:05:13 +0100

    Creando una cookie

--- 97-curso-max/11-tienda-mongoose/controllers/auth.js
+++ 97-curso-max/11-tienda-mongoose/controllers/auth.js
@@ -1,9 +1,16 @@
-const { reset } = require('nodemon');
-
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get('Cookie').split('=')[1];
+
   // Esta ruta se tiene que corresponder con el fichero pug
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
+    isAuthenticated: isLoggedIn,
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader('Set-Cookie', 'loggedIn=true');
+
+  res.redirect('/');
 };
