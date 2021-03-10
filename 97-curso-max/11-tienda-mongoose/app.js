38bea174012bfe4c303003568155e46dec96a0d6
cjgmj <cjgmj@hotmail.com>
2021-03-10 15:56:43 +0100

    Guardar sesión en MongoDB

--- 97-curso-max/11-tienda-mongoose/app.js
+++ 97-curso-max/11-tienda-mongoose/app.js
@@ -6,11 +6,20 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGOBD_URI =
+  'mongodb+srv://node:dqoOedTlawu9fJI4@cluster0.a2uzr.mongodb.net/shop?retryWrites=true&w=majority';
+
 const app = express();
+
+const store = new MongoDBStore({
+  uri: MONGOBD_URI,
+  collection: 'sessions',
+});
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -34,6 +43,7 @@ app.use(
     secret: 'my secret',
     resave: false,
     saveUninitialized: false,
+    store,
   })
 );
 
@@ -65,9 +75,7 @@ app.use('/', (req, res, next) => {
 });
 
 mongoose
-  .connect(
-    'mongodb+srv://node:dqoOedTlawu9fJI4@cluster0.a2uzr.mongodb.net/shop?retryWrites=true&w=majority'
-  )
+  .connect(MONGOBD_URI)
   .then(() => {
     User.findOne().then((user) => {
       if (!user) {
