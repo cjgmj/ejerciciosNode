4d7507ab0312873f3fdc0aa476775f90017bc993
cjgmj <cjgmj@hotmail.com>
2021-03-11 14:57:54 +0100

    Eliminar una cookie

--- 97-curso-max/11-tienda-mongoose/controllers/auth.js
+++ 97-curso-max/11-tienda-mongoose/controllers/auth.js
@@ -25,3 +25,9 @@ exports.postLogin = (req, res, next) => {
     })
     .catch((err) => console.log(err));
 };
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
+};
