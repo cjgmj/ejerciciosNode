411431309509d464bf78f32f7f50539692b4811e
cjgmj <cjgmj@hotmail.com>
2021-03-19 16:04:48 +0100

    Implementar lógica del token

--- 97-curso-max/11-tienda-mongoose/controllers/auth.js
+++ 97-curso-max/11-tienda-mongoose/controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 
@@ -122,5 +124,42 @@ exports.getReset = (req, res, next) => {
     path: '/reset',
     pageTitle: 'Reset Password',
     errorMessage: req.flash('error'),
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+
+    // Es necesario indicar hex para que convierta de hexadecimal a ASCII
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash('error', 'No account with that email found.');
+          return res.redirect('/reset');
+        }
+
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600;
+
+        return user.save();
+      })
+      .then(() => {
+        res.redirect('/');
+        return transporter.sendMail({
+          to: req.body.email,
+          from: 'info@shop.com',
+          subject: 'Password reset',
+          html: `
+            <p> You requested a password reset</p>
+            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+          `,
+        });
+      })
+      .catch((err) => console.log(err));
   });
 };
