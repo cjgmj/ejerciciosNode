478143c56230324899829bffaa5633ca403496b0
cjgmj <cjgmj@hotmail.com>
2021-04-01 18:35:47 +0200

    Servir archivos en stream

--- 97-curso-max/11-tienda-mongoose/controllers/shop.js
+++ 97-curso-max/11-tienda-mongoose/controllers/shop.js
@@ -179,18 +179,29 @@ exports.getInvoice = (req, res, next) => {
       const invoiceName = `invoice-${orderId}.pdf`;
       const invoicePath = path.join('data', 'invoices', invoiceName);
 
-      fs.readFile(invoicePath, (err, data) => {
-        if (err) {
-          return next(err);
-        }
+      // fs.readFile(invoicePath, (err, data) => {
+      //   if (err) {
+      //     return next(err);
+      //   }
 
-        res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader(
-          'Content-Disposition',
-          `inline; filename="${invoiceName}"`
-        );
-        res.send(data);
-      });
+      //   res.setHeader('Content-Type', 'application/pdf');
+      //   res.setHeader(
+      //     'Content-Disposition',
+      //     `inline; filename="${invoiceName}"`
+      //   );
+      //   res.send(data);
+      // });
+
+      const file = fs.createReadStream(invoicePath);
+
+      res.setHeader('Content-Type', 'application/pdf');
+      // res.setHeader(
+      //   'Content-Disposition',
+      //   `attachement; filename="${invoiceName}"`
+      // );
+      res.setHeader('Content-Disposition', `inline; filename="${invoiceName}"`);
+
+      file.pipe(res);
     })
     .catch((err) => next(err));
 };
