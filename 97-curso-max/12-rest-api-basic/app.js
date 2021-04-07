b9d77f98ac76940c393acf8b0d38dfdc47d344ef
cjgmj <cjgmj@hotmail.com>
2021-04-07 19:54:57 +0200

    Configurar CORS

--- 97-curso-max/12-rest-api-basic/app.js
+++ 97-curso-max/12-rest-api-basic/app.js
@@ -6,6 +6,19 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  // Para añadir varios dominios se deben separar por comas
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    // OPTIONS no es necesario añadirlo
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
