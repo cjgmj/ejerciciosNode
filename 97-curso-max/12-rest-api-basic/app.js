682709117c02b1c9d9ab0d1fdcbbfe55006c1c47
cjgmj <cjgmj@hotmail.com>
2021-04-13 15:03:40 +0200

    Validación inicio de sesión

--- 97-curso-max/12-rest-api-basic/app.js
+++ 97-curso-max/12-rest-api-basic/app.js
@@ -55,8 +55,9 @@ app.use((error, req, res, next) => {
 
   const status = error.statusCode || 500;
   const message = error.message;
+  const data = error.data;
 
-  res.status(status).json({ message });
+  res.status(status).json({ message, data });
 });
 
 mongoose
