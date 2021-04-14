3b94571d7a8b4ffd8d75f2fdca11e5d64a9822ea
cjgmj <cjgmj@hotmail.com>
2021-04-14 21:07:04 +0200

    Generar JWT

--- 97-curso-max/12-rest-api-basic/controllers/auth.js
+++ 97-curso-max/12-rest-api-basic/controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -70,6 +71,17 @@ exports.login = (req, res, next) => {
 
         throw error;
       }
+
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        'somesupersecretsecret',
+        { expiresIn: '1h' }
+      );
+
+      res.status(200).json({ token, userId: loadedUser._id.toString() });
     })
     .catch((err) => {
       if (!err.statusCode) {
