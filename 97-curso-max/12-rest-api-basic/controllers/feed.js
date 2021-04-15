7a7513eefa040566e7e5fc69e69efe720faba089
cjgmj <cjgmj@hotmail.com>
2021-04-15 14:59:17 +0200

    Conectar post con usuarios

--- 97-curso-max/12-rest-api-basic/controllers/feed.js
+++ 97-curso-max/12-rest-api-basic/controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -53,22 +54,31 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   const imageUrl = req.file.path.replace('\\', '/');
+  let creator;
 
   const post = new Post({
     title,
     content,
     imageUrl: imageUrl,
-    creator: {
-      name: 'admin',
-    },
+    creator: req.userId,
   });
 
   post
     .save()
     .then((result) => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+
+      return user.save();
+    })
+    .then(() => {
       res.status(201).json({
         message: 'Post created successfully!',
-        post: result,
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((err) => {
