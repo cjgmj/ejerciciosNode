34eb9998d2a7611fb8a13111758bed11d8e6fac1
cjgmj <cjgmj@hotmail.com>
2021-04-30 15:09:50 +0200

    Probar código asíncrono

--- 97-curso-max/17-test/test/auth-controller.js
+++ 97-curso-max/17-test/test/auth-controller.js
@@ -5,9 +5,26 @@ const User = require('../models/user');
 const AuthController = require('../controllers/auth');
 
 describe('Auth Controller - Login', function () {
-  it('should throw an error with code 500 if accessing the database fails', function () {
+  it('should throw an error with code 500 if accessing the database fails', function (done) {
     sinon.stub(User, 'findOne');
     User.findOne.throws();
+
+    const req = {
+      body: {
+        email: 'test@test.com',
+        password: 'tester',
+      },
+    };
+
+    AuthController.login(req, {}, () => {}).then((result) => {
+      expect(result).to.be.an('error');
+      expect(result).to.have.property('statusCode', 500);
+
+      // Para probar código asíncrono es necesario pasar por
+      // argumentos la función y llamarla una vez terminados
+      // los tests
+      done();
+    });
 
     User.findOne.restore();
   });
