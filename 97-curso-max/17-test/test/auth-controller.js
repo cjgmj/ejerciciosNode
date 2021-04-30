c4c4ac44b89e77bd7af23bc512955b7d30322d97
cjgmj <cjgmj@hotmail.com>
2021-04-30 16:01:08 +0200

    Pruebas con conexión a base de datos

--- 97-curso-max/17-test/test/auth-controller.js
+++ 97-curso-max/17-test/test/auth-controller.js
@@ -1,8 +1,6 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 
-const mongoose = require('mongoose');
-
 const User = require('../models/user');
 const AuthController = require('../controllers/auth');
 
@@ -29,21 +27,5 @@ describe('Auth Controller - Login', function () {
     });
 
     User.findOne.restore();
-  });
-
-  it('should send a response with a valid user status for and existing user', function (done) {
-    mongoose
-      .connect(
-        'mongodb+srv://node:dqoOedTlawu9fJI4@cluster0.a2uzr.mongodb.net/test-messages?retryWrites=true&w=majority'
-      )
-      .then(() => {
-        const user = new User({
-          email: 'test@test.com',
-          password: 'tester',
-          name: 'Test',
-          posts: [],
-        });
-      })
-      .catch((err) => console.log(err));
   });
 });
---
+++ 97-curso-max/17-test/test/user-controller.js
@@ -0,0 +1,50 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const mongoose = require('mongoose');
+
+const User = require('../models/user');
+const UserController = require('../controllers/user');
+
+describe('User Controller', function () {
+  it('should send a response with a valid user status for and existing user', function (done) {
+    mongoose
+      .connect(
+        'mongodb+srv://node:dqoOedTlawu9fJI4@cluster0.a2uzr.mongodb.net/test-messages?retryWrites=true&w=majority',
+        { useNewUrlParser: true, useUnifiedTopology: true }
+      )
+      .then(() => {
+        const user = new User({
+          email: 'test@test.com',
+          password: 'tester',
+          name: 'Test',
+          posts: [],
+          _id: '5c0f66b979af55031b34728a',
+        });
+
+        return user.save();
+      })
+      .then(() => {
+        const req = { userId: '5c0f66b979af55031b34728a' };
+        const res = {
+          statusCode: 500,
+          userStatus: null,
+          status: function (code) {
+            this.statusCode = code;
+            return this;
+          },
+          json: function (data) {
+            this.userStatus = data.status;
+          },
+        };
+
+        UserController.getStatus(req, res, () => {}).then(() => {
+          expect(res.statusCode).to.be.equal(200);
+          expect(res.userStatus).to.be.equal('I am new!');
+
+          done();
+        });
+      })
+      .catch((err) => console.log(err));
+  });
+});
