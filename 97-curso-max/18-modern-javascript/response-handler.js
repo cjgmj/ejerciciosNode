075eba17dc8d01327ea850039041ef484c61770f
cjgmj <cjgmj@hotmail.com>
2021-05-03 19:07:51 +0200

    Más de ES Modules

--- 97-curso-max/18-modern-javascript/response-handler.js
+++ 97-curso-max/18-modern-javascript/response-handler.js
@@ -1,10 +1,15 @@
 import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Se puede exportar directamente en la declaración añadiendo 'export' al principio
 export const resHandler = (req, res, next) => {
-  fs.readFile('my-page.html', 'utf8', (err, data) => {
-    res.send(data);
-  });
+  // Con el tipo a módulo no existen las variables globales
+  // por lo que no podemos usar __dirname
+  res.sendFile(path.join(__dirname, 'my-page.html'));
 };
 
 // Para exportar varias cosas se hace sin el 'default'
