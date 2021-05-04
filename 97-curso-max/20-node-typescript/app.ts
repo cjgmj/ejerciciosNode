dc8cb02e3ea19613a5789cb929b3406154ddd834
cjgmj <cjgmj@hotmail.com>
2021-05-04 16:32:33 +0200

    Rutas

--- 97-curso-max/20-node-typescript/app.ts
+++ 97-curso-max/20-node-typescript/app.ts
@@ -2,6 +2,10 @@
 // import express = require('express');
 import express from 'express';
 
+import todosRoutes from './routes/todos';
+
 const app = express();
+
+app.use(todosRoutes);
 
 app.listen(3000);
