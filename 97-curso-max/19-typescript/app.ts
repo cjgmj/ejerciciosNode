136a0ab77ce48d1e99c753fe2f2c0eccffe8702a
cjgmj <cjgmj@hotmail.com>
2021-05-04 15:26:42 +0200

    Objetos y arrays

--- 97-curso-max/19-typescript/app.ts
+++ 97-curso-max/19-typescript/app.ts
@@ -3,6 +3,9 @@ const num2Element = document.getElementById('num2') as HTMLInputElement;
 // Con la ! indica que la propiedad no será nula
 const buttonElement = document.querySelector('button')!;
 
+const numResults: number[] = [];
+const textResults: string[] = [];
+
 function add(num1: number | string, num2: number | string) {
   if (typeof num1 === 'number' && typeof num2 === 'number') {
     return num1 + num2;
@@ -18,6 +21,10 @@ function add(num1: number | string, num2: number | string) {
 // Se puede configurar para que no compile si tiene errores
 // console.log(add('1', '6'));
 
+function printResult(resultObj: { val: number; timestamp: Date }) {
+  console.log(resultObj.val);
+}
+
 buttonElement.addEventListener('click', () => {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
@@ -26,6 +33,12 @@ buttonElement.addEventListener('click', () => {
   const result = add(+num1, +num2);
   const stringResult = add(num1, num2);
 
-  console.log(result);
-  console.log(stringResult);
+  // console.log(result);
+  // console.log(stringResult);
+
+  numResults.push(result as number);
+  textResults.push(stringResult as string);
+
+  printResult({ val: result as number, timestamp: new Date() });
+  console.log(numResults, textResults);
 });
