e4d2ea237b18816a79ef331ec5d2045962f51e7a
cjgmj <cjgmj@hotmail.com>
2021-05-04 14:50:05 +0200

    Casteo de tipo

--- 97-curso-max/19-typescript/app.js
+++ 97-curso-max/19-typescript/app.js
@@ -1,5 +1,17 @@
+var num1Element = document.getElementById('num1');
+var num2Element = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
 function add(num1, num2) {
     return num1 + num2;
 }
-add(1, 6);
-add('1', '6');
+// console.log(add(1, 6));
+// Con tipos da error aunque compila
+// Se puede configurar para que no compile si tiene errores
+// console.log(add('1', '6'));
+buttonElement.addEventListener('click', function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    // Con el + delante convierte de string a number
+    var result = add(+num1, +num2);
+    console.log(result);
+});
--- 97-curso-max/19-typescript/app.ts
+++ 97-curso-max/19-typescript/app.ts
@@ -1,9 +1,23 @@
+const num1Element = document.getElementById('num1') as HTMLInputElement;
+const num2Element = document.getElementById('num2') as HTMLInputElement;
+const buttonElement = document.querySelector('button');
+
 function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-console.log(add(1, 6));
+// console.log(add(1, 6));
 
 // Con tipos da error aunque compila
 // Se puede configurar para que no compile si tiene errores
 // console.log(add('1', '6'));
+
+buttonElement.addEventListener('click', () => {
+  const num1 = num1Element.value;
+  const num2 = num2Element.value;
+
+  // Con el + delante convierte de string a number
+  const result = add(+num1, +num2);
+
+  console.log(result);
+});
