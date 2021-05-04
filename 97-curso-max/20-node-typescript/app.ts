5c358199592b498928d2d937580dc105ac990005
cjgmj <cjgmj@hotmail.com>
2021-05-04 15:56:02 +0200

    Configurar aplicación con TypeScript

---
+++ 97-curso-max/20-node-typescript/app.ts
@@ -0,0 +1,7 @@
+// Usando ts se cambia la forma de importar, pudiendo usar las dos siguientes
+// import express = require('express');
+import express from 'express';
+
+const app = express();
+
+app.listen(3000);
