ac2d7b4d308051be6df08c08f5c3314f545ba60d
cjgmj <cjgmj@hotmail.com>
2021-05-04 14:42:39 +0200

    Asignando tipos

--- 97-curso-max/19-typescript/app.ts
+++ 97-curso-max/19-typescript/app.ts
@@ -1,7 +1,9 @@
-function add(num1, num2) {
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
 console.log(add(1, 6));
 
-console.log(add('1', '6'));
+// Con tipos da error aunque compila
+// Se puede configurar para que no compile si tiene errores
+// console.log(add('1', '6'));
