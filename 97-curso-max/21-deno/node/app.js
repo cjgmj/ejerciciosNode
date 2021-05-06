33c345b6a677aea3ccd83feef3b278ecac0b44d0
cjgmj <cjgmj@hotmail.com>
2021-05-06 18:34:35 +0200

    REST API node

---
+++ 97-curso-max/21-deno/node/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const todoRoutes = require('./routes/todos');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(todoRoutes);
+
+app.listen(3000);
