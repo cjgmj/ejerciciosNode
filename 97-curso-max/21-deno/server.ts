ee0a80f73e969f4f119afa65f2df728c1329eada
cjgmj <cjgmj@hotmail.com>
2021-05-06 15:51:57 +0200

    Standard Library

---
+++ 97-curso-max/21-deno/server.ts
@@ -0,0 +1,9 @@
+// deno run --allow-net server.ts
+
+import { serve } from 'https://deno.land/std/http/server.ts';
+
+const server = serve({ port: 3000 });
+
+for await (const req of server) {
+  req.respond({ body: 'Hello World' });
+}
